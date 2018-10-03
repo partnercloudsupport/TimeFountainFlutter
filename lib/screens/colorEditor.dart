06838a62cfb8e96952cb6ab7dee8efdec6797c4a
Jakob Staudinger <jakobstaudi@gmail.com>
2018-10-03 17:10:17 +0200

    Changed appearance of ColorConfigurations in Profile Editor, made ColorEditor functional

---
+++ lib/screens/colorEditor.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import '../model/colorConfigurationDTO.dart';
+
+class ColorEditorScreen extends StatefulWidget {
+  final ColorConfigurationDTO colorConfigurationDTO;
+  ColorEditorScreen(this.colorConfigurationDTO);
+  @override
+  ColorState createState() => new ColorState(colorConfigurationDTO);
+}
+
+class ColorState extends State<ColorEditorScreen> {
+  ColorConfigurationDTO colorConfiguration;
+  ColorConfigurationDTO colorConfigurationCopy;
+  ColorState(this.colorConfiguration)
+      : colorConfigurationCopy = new ColorConfigurationDTO(
+            colorConfiguration.red,
+            colorConfiguration.green,
+            colorConfiguration.blue,
+            colorConfiguration.frequencyDelta,
+            colorConfiguration.offset,
+            colorConfiguration.amplitude,
+            colorConfiguration.behaviour,
+            colorConfiguration.flashDuration);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          GestureDetector(
+              onTap: () { },
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: Container(
+                  height: 32.0,
+                  color: Color.fromARGB(
+                      255,
+                      colorConfigurationCopy.red,
+                      colorConfigurationCopy.green,
+                      colorConfigurationCopy.blue),
+                ))
+              ])),
+          _buildInputField(
+              'ΔFrequency',
+              (String value) => _setFrequency(colorConfigurationCopy, value),
+              colorConfigurationCopy.frequencyDelta.toString()),
+          _buildInputField(
+              'Offset',
+              (String value) => _setOffset(colorConfigurationCopy, value),
+              colorConfigurationCopy.offset.toString()),
+          _buildInputField(
+              'Amplitude',
+              (String value) => _setAmplitude(colorConfigurationCopy, value),
+              colorConfigurationCopy.amplitude.toString()),
+          _buildInputField(
+              'FlashDuration',
+              (String value) =>
+                  _setFlashDuration(colorConfigurationCopy, value),
+              colorConfigurationCopy.flashDuration.toString()),
+          _buildBehaviourDropdown(colorConfigurationCopy)
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('OK'),
+          onPressed: () {
+            colorConfiguration.amplitude = colorConfigurationCopy.amplitude;
+            colorConfiguration.offset = colorConfigurationCopy.offset;
+            colorConfiguration.frequencyDelta =
+                colorConfigurationCopy.frequencyDelta;
+            colorConfiguration.flashDuration =
+                colorConfigurationCopy.flashDuration;
+            colorConfiguration.behaviour = colorConfigurationCopy.behaviour;
+            colorConfiguration.red = colorConfigurationCopy.red;
+            colorConfiguration.green = colorConfigurationCopy.green;
+            colorConfiguration.blue = colorConfigurationCopy.blue;
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
+  }
+
+  void _setFrequency(ColorConfigurationDTO colorConfiguration, String value) {
+    double val = double.tryParse(value);
+    if (val != null) {
+      colorConfiguration.frequencyDelta = val;
+    }
+  }
+
+  void _setOffset(ColorConfigurationDTO colorConfiguration, String value) {
+    double val = double.tryParse(value);
+    if (val != null) {
+      colorConfiguration.offset = val;
+    }
+  }
+
+  void _setAmplitude(ColorConfigurationDTO colorConfiguration, String value) {
+    double val = double.tryParse(value);
+    if (val != null) {
+      colorConfiguration.amplitude = val;
+    }
+  }
+
+  void _setFlashDuration(
+      ColorConfigurationDTO colorConfiguration, String value) {
+    int val = int.tryParse(value);
+    if (val != null) {
+      colorConfiguration.flashDuration = val;
+    }
+  }
+
+  Widget _buildInputField(String hint, onChanged, String initialValue) {
+    return Padding(
+        padding: EdgeInsets.only(left: 16.0, top: 12.0, bottom: 12.0),
+        child: TextField(
+          decoration: InputDecoration(hintText: hint),
+          onChanged: onChanged,
+          controller: TextEditingController(text: initialValue),
+        ));
+  }
+
+  Widget _buildBehaviourDropdown(ColorConfigurationDTO colorConfiguration) {
+    return DropdownButton<ColorBehaviour>(
+        items: [
+          DropdownMenuItem(value: ColorBehaviour.linear, child: Text('Linear')),
+          DropdownMenuItem(value: ColorBehaviour.sine, child: Text('Sine'))
+        ],
+        onChanged: (ColorBehaviour value) {
+          setState(() {
+            colorConfiguration.behaviour = value;
+          });
+        },
+        hint: Text('Behaviour'),
+        value: colorConfiguration.behaviour);
+  }
+}
--- lib/screens/profileEditor.dart
+++ lib/screens/profileEditor.dart
@@ -1,27 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import '../model/timeFountainDTO.dart';
+import '../model/colorConfigurationDTO.dart';
+import '../model/profileDTO.dart';
+import './colorEditor.dart';
 
 class ProfileEditorScreen extends StatefulWidget {
   final BluetoothDevice device;
   final TimeFountainDTO timeFountainDTO;
   final int index;
 
-ProfileEditorScreen(this.device, this.timeFountainDTO, this.index);
+  ProfileEditorScreen(this.device, this.timeFountainDTO, this.index);
 
   @override
-  ProfileState createState() => new ProfileState(device, timeFountainDTO, index);
+  ProfileState createState() =>
+      new ProfileState(device, timeFountainDTO, index);
 }
 
 class ProfileState extends State<ProfileEditorScreen> {
   final BluetoothDevice device;
   final TimeFountainDTO timeFountainDTO;
-  final int index;
+  final int profileIndex;
 
-  ProfileState(this.device, this.timeFountainDTO, this.index);
+  ProfileState(this.device, this.timeFountainDTO, this.profileIndex);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Profile Editor')),);
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile Editor')),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _addColorConfiguration, child: Icon(Icons.add)),
+    );
+  }
+
+  ProfileDTO getProfile() {
+    return timeFountainDTO.profiles.elementAt(profileIndex);
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        int index = i ~/ 2;
+        if (index >= getProfile().colorConfigurationDTO.length) {
+          return null;
+        }
+
+        ColorConfigurationDTO colorConfiguration =
+            getProfile().colorConfigurationDTO.elementAt(index);
+
+        return ListTile(
+            title: Row(
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: 16.0, top: 8.0, bottom: 8.0, right: 16.0),
+                    child: Image(
+                        height: 42.0,
+                        image: AssetImage('assets/raindrop.png'),
+                        color: Color.fromARGB(
+                            255,
+                            colorConfiguration.red,
+                            colorConfiguration.green,
+                            colorConfiguration.blue))),
+                Text(
+                  'Color ${index + 1}',
+                ),
+              ],
+            ),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) => ColorEditorScreen(colorConfiguration));
+            });
+      },
+    );
+  }
+
+  void _addColorConfiguration() {
+    setState(() {
+      timeFountainDTO.profiles
+          .elementAt(profileIndex)
+          .colorConfigurationDTO
+          .add(ColorConfigurationDTO(
+              255, 0, 0, 0.0, 0.0, 1.0, ColorBehaviour.linear, 1500));
+    });
   }
 }
